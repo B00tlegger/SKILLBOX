310716b67b538d61dc3c18a798b0ae1c63616665
B00tlegger <shmula495@gmail.com>
2021-10-23 14:48:13 +0300

    Create Main.java

---
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Container container = new Container();
+    container.count += 7843;
+
+    int sum = sumDigits(7843);
+
+    System.out.println(sum);
+  }
+
+  /* Реализуйте метод sumDigits который возвращает сумму цифр числа, пример:
+  передано 12345, метод должен вернуть 15
+  если передано null, то должен вернуть -1
+
+  Запустите тесты TestSumDigits для проверки корректности работы метода
+
+  не меняйте название метода, его возвращаемое значение и модификаторы доступа (public).
+  В противном случае тестовый метод не сможет проверить ваш код
+   */
+
+  public static int sumDigits(Integer number) {
+    int sum = 0;
+    if (number == null){
+      sum = -1;
+    }else {
+      String Number = Integer.toString(number);
+      for (int i = 0; i < Number.length(); i++) {
+        sum = sum + Integer.parseInt(String.valueOf(Number.charAt(i)));
+      }
+    }
+    return sum;
+  }
+}
