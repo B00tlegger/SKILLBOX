c1191d7f1b6c98a7d9691a24b4305cf5c18ffa90
B00tlegger <shmula495@gmail.com>
2021-10-23 14:48:38 +0300

    Create main.java

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/main.java
@@ -0,0 +1,14 @@
+public class main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("byte от " + Byte.MAX_VALUE + " до " + Byte.MIN_VALUE);
+        System.out.println("int от " + Integer.MAX_VALUE + " до " + Integer.MAX_VALUE);
+        System.out.println("short от " + Short.MAX_VALUE + " до " + Short.MIN_VALUE);
+        System.out.println("long от " + Long.MAX_VALUE + " до " + Long.MIN_VALUE);
+        System.out.println("double от " + Double.MAX_VALUE + " до " +  Double.MIN_VALUE);
+        System.out.println("float от " + Float.MAX_VALUE + " до " + Float.MIN_VALUE);
+    }
+
+}
